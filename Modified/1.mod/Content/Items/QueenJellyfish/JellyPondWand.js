84b580581b5c31e7b07dc90c3538a068cba3cf22
potatooo2 <108599125+potatooo2@users.noreply.github.com>
2026-07-06 02:45:53 -0300

    finished queenjellyfish drops

--- Modified/1.mod/Content/Items/QueenJellyfish/JellyPondWand.js
+++ Modified/1.mod/Content/Items/QueenJellyfish/JellyPondWand.js
@@ -16,20 +16,16 @@ export class JellyPondWand extends ModItem {
     SetDefaults() {
         this.Item.magic = true;
         this.Item.noMelee = true;
-
         this.Item.width = 40;
         this.Item.height = 40;
-
         this.Item.mana = 11;
 
         this.SetWeaponValues(21, 3, 4);
         this.SetDefaultWeaponStyle(26, true);
 
         this.Item.UseSound = Terraria.ID.SoundID.Item43;
-
         this.Item.shoot = ModProjectile.getTypeByName("JellySpawnPro");
         this.Item.shootSpeed = 8;
-
         this.Item.value = Terraria.Item.sellPrice(0, 0, 30, 0);
         this.Item.rare = Terraria.ID.ItemRarityID.Blue;
     }
